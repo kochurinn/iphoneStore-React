f8271e8139a54eeef6dbc6764d6a0379c93a783d
kochurinn <kochurinn@yandex.ru>
2024-04-01 21:09:35 +0500

    iphoneStore-React
    iphoneStore-React

--- src/App.js
+++ src/App.js
@@ -2,6 +2,29 @@ import Cart from './components/Cart'
 import Header from './components/Header'
 import Card from './components/Card'
 
+const objCard = [
+  {
+    title: 'Iphone 15 Pro Max 128 гб Black',
+    price: 109000,
+    imgUrl: './img/ip15proMaxBlackTitan.webp'
+  },
+  {
+    title: 'Iphone 15 Pro 128 гб Black',
+    price: 101000,
+    imgUrl: './img/ip15proBlackTitan.webp'
+  },
+  {
+    title: 'Iphone 15 Pro 128 гб White',
+    price: 102000,
+    imgUrl: './img/ip15proWhite.webp'
+  },
+  {
+    title: 'Iphone 14 128 гб Blue',
+    price: 72000,
+    imgUrl: './img/ip14blue.webp'
+  },
+]
+
 function App() {
   return (
     <div className="wrapper">
@@ -16,7 +39,9 @@ function App() {
           </div>
         </div>
         <div className="phones">
-          <Card />
+          {objCard.map(card => {
+            return <Card title={card.title} price={card.price} img={card.imgUrl} />
+          })}
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,23 @@
-function Card() {
-    return (
-        <div className="phones__elem">
-            <img className="phones__elem-productImg" src="/img/ip15proMaxBlackTitan.webp" alt="" />
-            <p>Iphone 15 Pro Max <br /> 128 гб Black</p>
-            <div className="phones__elem-price">
-              <div>
-                <span>ЦЕНА:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button>
-                <img src="/img/plus.svg" alt="" />
-              </button>
-            </div>
+function Card(props) {
+
+  const onClickPlus = () => {
+    alert('Вы нажали на плюсик')
+  }
+
+  return (
+      <div className="phones__elem">
+        <img className="phones__elem-productImg" src={props.img} alt="" />
+        <p>{props.title}</p>
+        <div className="phones__elem-price">
+          <div>
+            <span>ЦЕНА:</span>
+            <b>{props.price} руб.</b>
           </div>
-    )
+          <button onClick={onClickPlus}>
+            <img src="/img/plus.svg" alt="" />
+          </button>
+        </div>
+      </div>
+  )
 }
 export default Card
